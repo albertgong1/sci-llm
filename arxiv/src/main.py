f97cedb39d962827cffd7f92152b60997a26450c
tempoxylophone <15273090+tempoxylophone@users.noreply.github.com>
2025-12-04 23:39:45 -0500

    add modifcations for charge density papers

--- arxiv/src/main.py
+++ arxiv/src/main.py
@@ -1,5 +1,7 @@
 import csv
 import re
+from pathlib import Path
+
 import pandas as pd
 
 from src.config import DATASET_ROOT
@@ -41,13 +43,9 @@ def get_proper_doi_from_doi_urls(doi_urls: list[str]) -> list[str]:
     return collection
 
 
-if __name__ == "__main__":
-    SOURCE_CSV = DATASET_ROOT / "SuperConDOI.csv"
-    # turn to None for all
-    TESTING_LIMIT = None
-
+def get_super_con_papers(csv_path: Path = DATASET_ROOT / "SuperConDOI.csv"):
     parsed = []
-    with open(SOURCE_CSV, newline="\n", encoding="utf-8") as f:
+    with open(csv_path, newline="\n", encoding="utf-8") as f:
         reader = csv.reader(f)
         # skip header
         _ = next(reader)
@@ -63,15 +61,72 @@ if __name__ == "__main__":
                 }
             )
 
-    all_doi_urls = [x["doi_link"] for x in parsed]
+    return parsed
+
+
+def get_real_doi_from_base_doi(parsed_objects: list[dict[str, str]]):
+    all_doi_urls = [x["doi_link"] for x in parsed_objects]
     true_doi_values = get_proper_doi_from_doi_urls(all_doi_urls)
-    assert len(parsed) == len(true_doi_values)
+    assert len(parsed_objects) == len(true_doi_values)
 
     # now all entries have their 'true doi'
-    for p, true_doi in zip(parsed, true_doi_values):
+    for p, true_doi in zip(parsed_objects, true_doi_values):
         p["true_doi"] = true_doi
 
-    print("Done getting true DOIs")
+    return parsed_objects
+
+
+def get_super_con_arxiv_info(testing_limit: int | None):
+    super_con_papers = get_super_con_papers()
+    parsed = get_real_doi_from_base_doi(super_con_papers)
+    ordered_papers = list(
+        sorted(parsed, key=lambda x: x["published_year"], reverse=True)
+    )[:testing_limit]
+
+    # using the true dois, search for them
+    returned_results = get_arxiv_search_results_by_dois(
+        [x["true_doi"] for x in ordered_papers]
+    )
+
+    normed = []
+    for paper_info, arxiv_search_results in zip(ordered_papers, returned_results):
+        for arxiv_search_result in arxiv_search_results:
+            to_save = {
+                **paper_info,
+                **arxiv_search_result,
+            }
+            normed.append(to_save)
+
+    df = pd.json_normalize(normed)
+    df.to_csv(DATASET_ROOT / "supercon_augmented_search_results.csv", index=False)
+
+
+def get_charge_density_wave_papers(
+    csv_path: Path = DATASET_ROOT / "Charge_Density_Wave_Paper_list_Sheet1.csv",
+) -> list[dict[str, str]]:
+    parsed = []
+    with open(csv_path, newline="\n", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        # skip header
+        _ = next(reader)
+        for row in reader:
+            paper_title, doi_link, published_year = row
+            parsed.append(
+                {
+                    "paper_title": paper_title,
+                    "doi_link": doi_link,
+                    "published_year": int(published_year),
+                }
+            )
+    return parsed
+
+
+if __name__ == "__main__":
+    # get_super_con_arxiv_info()
+    papers = get_charge_density_wave_papers()
+    parsed = get_real_doi_from_base_doi(papers)
+
+    TESTING_LIMIT = None
     ordered_papers = list(
         sorted(parsed, key=lambda x: x["published_year"], reverse=True)
     )[:TESTING_LIMIT]
@@ -91,4 +146,7 @@ if __name__ == "__main__":
             normed.append(to_save)
 
     df = pd.json_normalize(normed)
-    df.to_csv(DATASET_ROOT / "supercon_augmented_search_results.csv", index=False)
+    df.to_csv(
+        DATASET_ROOT / "charge_density_wave_paper_list_sheet_1_augmented.csv",
+        index=False,
+    )
