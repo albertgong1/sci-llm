4b70984b18580b5c730beb776e2f29a3c9c0d829
Albert Gong <ag2435@cornell.edu>
2025-12-10 17:42:52 -0500

    moved utils.py to src

---
+++ src/pbench_eval/__init__.py
@@ -0,0 +1,15 @@
+"""Property Benchmark Evaluation utilities."""
+
+from pbench_eval.utils import (
+    normalize_formula,
+    scorer_categorical,
+    scorer_pymatgen,
+    scorer_si,
+)
+
+__all__ = [
+    "normalize_formula",
+    "scorer_categorical",
+    "scorer_pymatgen",
+    "scorer_si",
+]
