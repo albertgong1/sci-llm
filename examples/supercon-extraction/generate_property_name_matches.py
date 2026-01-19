e1f8df93a52c006aff76502bd86040eb76df7eab
Albert Gong <ag2435@cornell.edu>
2026-01-19 17:16:10 -0500

    check whether gt_matches and pred_matches exist separately

--- examples/supercon-extraction/generate_property_name_matches.py
+++ examples/supercon-extraction/generate_property_name_matches.py
@@ -160,28 +160,6 @@ async def main(args: argparse.Namespace) -> None:
         logger.info(f"Processing refno {refno}...")
         # Get predicted properties for this refno
         df_pred = df[df["refno"] == refno]
-        # Obtain the ground truth properties for this refno
-        # import pdb; pdb.set_trace()
-        df_gt_refno = df_gt[df_gt["refno"].str.lower() == refno.lower()].drop(
-            columns=["refno"]
-        )
-
-        pred_matches_path = (
-            pred_matches_dir / f"pred_matches_{refno}_{model_name}_k{top_k}.csv"
-        )
-        pred_responses_path = (
-            pred_responses_dir / f"pred_responses_{refno}_{model_name}_k{top_k}.csv"
-        )
-        gt_matches_path = (
-            gt_matches_dir / f"gt_matches_{refno}_{model_name}_k{top_k}.csv"
-        )
-        gt_responses_path = (
-            gt_responses_dir / f"gt_responses_{refno}_{model_name}_k{top_k}.csv"
-        )
-        if pred_matches_path.exists() and gt_matches_path.exists() and not force:
-            logger.info(f"Skipping refno {refno} because pred matches already exist")
-            continue
-
         # Obtain the embeddings for the predicted properties
         df_pred_embeddings = pd.read_parquet(pred_embeddings_dir / f"{refno}.parquet")
         # Join df_pred and df_pred_embeddings on property_name
@@ -192,47 +170,70 @@ async def main(args: argparse.Namespace) -> None:
         )
         # construct the context from location.evidence
         df_pred["context"] = df_pred["location.evidence"]
+        # Obtain the ground truth properties for this refno
+        # import pdb; pdb.set_trace()
+        df_gt_refno = df_gt[df_gt["refno"].str.lower() == refno.lower()].drop(
+            columns=["refno"]
+        )
 
         # -- Get top-k matches for each predicted property name (to compute precision) --
-        df_pred_matches, df_pred_responses = await generate_property_name_matches(
-            df_pred,
-            df_gt_refno,
-            llm,
-            inf_gen_config,
-            prompt_template,
-            top_k=top_k,
-            left_on=["property_name", "context"],
-            right_on=["property_name", "context"],
-            left_suffix="_pred",
-            right_suffix="_gt",
+        pred_matches_path = (
+            pred_matches_dir / f"pred_matches_{refno}_{model_name}_k{top_k}.csv"
         )
-        # import pdb; pdb.set_trace()
-        df_pred_matches.drop(columns=["embedding_pred", "embedding_gt"]).to_csv(
-            pred_matches_path, index=False
+        pred_responses_path = (
+            pred_responses_dir / f"pred_responses_{refno}_{model_name}_k{top_k}.csv"
         )
-        logger.info(f"Saved pred matches to {pred_matches_path}")
-        df_pred_responses.to_csv(pred_responses_path, index=False)
-        logger.info(f"Saved pred responses to {pred_responses_path}")
+        if pred_matches_path.exists() and not force:
+            logger.info(f"Skipping refno {refno} because pred matches already exist")
+        else:
+            df_pred_matches, df_pred_responses = await generate_property_name_matches(
+                df_pred,
+                df_gt_refno,
+                llm,
+                inf_gen_config,
+                prompt_template,
+                top_k=top_k,
+                left_on=["property_name", "context"],
+                right_on=["property_name", "context"],
+                left_suffix="_pred",
+                right_suffix="_gt",
+            )
+            # import pdb; pdb.set_trace()
+            df_pred_matches.drop(columns=["embedding_pred", "embedding_gt"]).to_csv(
+                pred_matches_path, index=False
+            )
+            logger.info(f"Saved pred matches to {pred_matches_path}")
+            df_pred_responses.to_csv(pred_responses_path, index=False)
+            logger.info(f"Saved pred responses to {pred_responses_path}")
 
         # -- Get top-k matches for each ground truth property name (to compute recall) --
-        df_gt_matches, df_gt_responses = await generate_property_name_matches(
-            df_gt_refno,
-            df_pred,
-            llm,
-            inf_gen_config,
-            prompt_template,
-            top_k=top_k,
-            left_on=["property_name", "context"],
-            right_on=["property_name", "context"],
-            left_suffix="_gt",
-            right_suffix="_pred",
+        gt_matches_path = (
+            gt_matches_dir / f"gt_matches_{refno}_{model_name}_k{top_k}.csv"
         )
-        df_gt_matches.drop(columns=["embedding_gt", "embedding_pred"]).to_csv(
-            gt_matches_path, index=False
+        gt_responses_path = (
+            gt_responses_dir / f"gt_responses_{refno}_{model_name}_k{top_k}.csv"
         )
-        logger.info(f"Saved gt matches to {gt_matches_path}")
-        df_gt_responses.to_csv(gt_responses_path, index=False)
-        logger.info(f"Saved gt responses to {gt_responses_path}")
+        if gt_matches_path.exists() and not force:
+            logger.info(f"Skipping refno {refno} because gt matches already exist")
+        else:
+            df_gt_matches, df_gt_responses = await generate_property_name_matches(
+                df_gt_refno,
+                df_pred,
+                llm,
+                inf_gen_config,
+                prompt_template,
+                top_k=top_k,
+                left_on=["property_name", "context"],
+                right_on=["property_name", "context"],
+                left_suffix="_gt",
+                right_suffix="_pred",
+            )
+            df_gt_matches.drop(columns=["embedding_gt", "embedding_pred"]).to_csv(
+                gt_matches_path, index=False
+            )
+            logger.info(f"Saved gt matches to {gt_matches_path}")
+            df_gt_responses.to_csv(gt_responses_path, index=False)
+            logger.info(f"Saved gt responses to {gt_responses_path}")
 
 
 if __name__ == "__main__":
