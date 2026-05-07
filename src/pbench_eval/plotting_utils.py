7c3c43f453fbeb9f9fa2774e428cc55334063265
Albert Gong <ag2435@cornell.edu>
2026-05-06 20:38:02 -0400

    use sans serif

--- src/pbench_eval/plotting_utils.py
+++ src/pbench_eval/plotting_utils.py
@@ -1,7 +1,5 @@
-import matplotlib.pyplot as plt
-
-plt.rcParams["font.family"] = "serif"
-plt.rcParams["font.serif"] = ["Times New Roman"]
+# plt.rcParams["font.family"] = "serif"
+# plt.rcParams["font.serif"] = ["Times New Roman"]
 
 # Single column figures
 TICK_FONT_SIZE = 6
